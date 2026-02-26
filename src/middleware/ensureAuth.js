abef1b6e88295c0f0f26c909bb75e71c141b3341
Lora Chisholm <chi20006@byui.edu>
2026-02-26 01:36:46 -0700

    Protect POST PUT DELETE routes with ensureAuth middleware

---
+++ src/middleware/ensureAuth.js
@@ -0,0 +1,11 @@
+function ensureAuth(req, res, next) {
+  if (req.isAuthenticated && req.isAuthenticated()) {
+    return next();
+  }
+
+  return res.status(401).json({
+    message: "Unauthorized. Please log in with Google."
+  });
+}
+
+module.exports = ensureAuth;
--- src/routes/events.js
+++ src/routes/events.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const eventsController = require("../controllers/eventsController");
+const ensureAuth = require("../middleware/ensureAuth");
 
+// Public Routes
 router.get("/", eventsController.getAllEvents);
 router.get("/:id", eventsController.getEventById);
-router.post("/", eventsController.createEvent);
-router.put("/:id", eventsController.updateEvent);
-router.delete("/:id", eventsController.deleteEvent);
+
+// Protected Routes (Require Login)
+router.post("/", ensureAuth, eventsController.createEvent);
+router.put("/:id", ensureAuth, eventsController.updateEvent);
+router.delete("/:id", ensureAuth, eventsController.deleteEvent);
 
 module.exports = router;
