c5f36504d38bf795008e1846f88814c9af9f5847
Lora Chisholm <chi20006@byui.edu>
2026-02-26 01:24:36 -0700

    Add schema fields to User model

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    googleId: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    displayName: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
